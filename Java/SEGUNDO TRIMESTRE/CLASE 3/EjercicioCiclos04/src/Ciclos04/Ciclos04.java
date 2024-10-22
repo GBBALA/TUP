351489440314d7c6eca45d11166be1db093c992a
Javier Quiroga <elgrosojavi@gmail.com>
2024-10-22 17:07:33 -0300

    clase 3

--- Java/SEGUNDO TRIMESTRE/CLASE 3/EjercicioCiclos04/src/Ciclos04/Ciclos04.java
+++ Java/SEGUNDO TRIMESTRE/CLASE 3/EjercicioCiclos04/src/Ciclos04/Ciclos04.java
@@ -1,2 +1,23 @@
-package Ciclos04;public class Ciclos04 {
+package Ciclos04;
+
+import java.util.Scanner;
+public class Ciclos04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        int contador = 0;
+
+        // Pedir números hasta que se teclee uno negativo
+        System.out.println("Introduce un número (número negativo para salir):");
+        numero = scanner.nextInt();
+
+        while (numero >= 0) {
+            contador++;  // Incrementar el contador por cada número válido ingresado
+            System.out.println("Introduce otro número (número negativo para salir):");
+            numero = scanner.nextInt();
+        }
+
+        System.out.println("Se han introducido " + contador + " números.");
+        scanner.close();
+    }
 }
---
+++ Java/SEGUNDO TRIMESTRE/CLASE 3/EjercicioCiclos04/src/Ciclos04/Ejercicio04.java
@@ -0,0 +1,21 @@
+package Ciclos04;
+import javax.swing.JOptionPane;
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        int numero;
+        int contador = 0;
+        String input;
+
+        // Pedir números hasta que se teclee uno negativo
+        input = JOptionPane.showInputDialog("Introduce un número (número negativo para salir):");
+        numero = Integer.parseInt(input);
+
+        while (numero >= 0) {
+            contador++;  // Incrementar el contador por cada número válido ingresado
+            input = JOptionPane.showInputDialog("Introduce otro número (número negativo para salir):");
+            numero = Integer.parseInt(input);
+        }
+
+        JOptionPane.showMessageDialog(null, "Se han introducido " + contador + " números.");
+    }
+}
