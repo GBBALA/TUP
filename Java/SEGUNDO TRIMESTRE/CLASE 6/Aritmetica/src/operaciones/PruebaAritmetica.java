94ac19edc793fc8181ad54a426fa76e87fcf41e6
Javier Quiroga <elgrosojavi@gmail.com>
2024-10-22 17:05:48 -0300

    clase 6

--- Java/SEGUNDO TRIMESTRE/CLASE 6/Aritmetica/src/operaciones/PruebaAritmetica.java
+++ Java/SEGUNDO TRIMESTRE/CLASE 6/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -8,7 +8,7 @@ public class PruebaAritmetica {
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b =7;
-        aritmetica1 = null;
+        //aritmetica1 = null;
         aritmetica1.sumarNumeros ();
 
         //Para almacenar un objeto o los atributos wse utiliza la memoria heap
